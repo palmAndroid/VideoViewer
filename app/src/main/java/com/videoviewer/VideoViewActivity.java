d0a91700450235505d523820d4974c29b0c8da88
ankita shukla <ankita.shukla@intelegencia.com>
2017-07-04 16:07:14 +0530

    Added VideoViewActivity.java

---
+++ app/src/main/java/com/videoviewer/VideoViewActivity.java
@@ -0,0 +1,18 @@
+package com.videoviewer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ankita.shukla on 7/4/2017.
+ */
+
+public class VideoViewActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
