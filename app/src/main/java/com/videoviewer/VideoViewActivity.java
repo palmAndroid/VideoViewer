d24276d8ddfa71fc0539082e2e7fc33ce7b73cb2
ankita shukla <ankita.shukla@intelegencia.com>
2017-07-04 16:17:01 +0530

    Added VideoViewActivity.java Again

--- app/src/main/java/com/videoviewer/VideoViewActivity.java
+++ app/src/main/java/com/videoviewer/VideoViewActivity.java
@@ -3,16 +3,17 @@ package com.videoviewer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 /**
  * Created by ankita.shukla on 7/4/2017.
  */
 
-public class VideoViewActivity extends AppCompatActivity{
-
+public class VideoViewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(getApplicationContext(),"Hi From VideoViewActivity", Toast.LENGTH_LONG).show();
     }
 }
